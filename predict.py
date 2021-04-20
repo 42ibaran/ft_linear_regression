43da21f381515d2e3a9fe002f24ce380bb93d2be
Ivan Baran <ibaran@student.42.fr>
2021-04-20 11:34:57 +0000

    handle negatives in predict

--- predict.py
+++ predict.py
@@ -12,8 +12,15 @@ except ValueError:
     log.error("Invalid mileage value.")
     exit(1)
 
+if mileage < 0:
+    log.error("Negative mileage, really?")
+    exit(1)
+
 try:
     prediction = model.predict(mileage)
+    if prediction < 0:
+        log.warning("Predicted price is negative, defaulting to 0.")
+        prediction = 0
     print(int(prediction))
 except ValueError:
     log.error("Unable to predict, try training again.")
